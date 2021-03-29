a99d91f42c7d8af4c0151b0eecccc8b95358e353
Mrzhouqifei <qifeizhou@pku.edu.cn>
2021-03-29 21:15:19 +0800

    final deliverable

--- update_data.py
+++ update_data.py
@@ -1,21 +1,20 @@
 from preprocess.download_data import download_covid_data, download_policy, policy_preprocess
 import time
 
-# for series_category in ['confirmed', 'deaths', 'recovered']:
-#     try:
-#         download_covid_data(series_category)
-#         print('download success for %s' % series_category)
-#     except:
-#         print('download fail for %s' % series_category)
-#     time.sleep(5)
-#
-#
-# try:
-#     download_policy()
-#     print('download success for policy')
-#     # process the raw policies
-#     policy_preprocess()
-# except:
-#     print('download false for policy')
-#
-policy_preprocess()
+for series_category in ['confirmed', 'deaths', 'recovered']:
+    try:
+        download_covid_data(series_category)
+        print('download success for %s' % series_category)
+    except:
+        print('download fail for %s' % series_category)
+    time.sleep(5)
+
+try:
+    download_policy()
+    print('download success for policy')
+    # process the raw policies
+    policy_preprocess()
+except:
+    print('download false for policy')
+
+# policy_preprocess()

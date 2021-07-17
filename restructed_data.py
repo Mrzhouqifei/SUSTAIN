ffd414c31a9969c19d30b886c53eb9f10a57fa9d
zhouqifei <qifeizhou@pku.edu.cn>
2021-07-17 21:10:34 +0800

    0717

--- restructed_data.py
+++ restructed_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import json
 import os
 
 
@@ -56,6 +57,26 @@ def mixed_policies_forecast(business_policy_intensity, mixed_policy_intensity,
         mixed_policy_series.append(y)
         pre_y = y
     return mixed_policy_series
+
+
+def re_construct_policy_rank_score():
+    policy_rank_score = pd.read_csv('output/Final results/compiled_policy ranking and scores.csv')
+    for key, group in policy_rank_score.groupby('policy'):
+        if not os.path.exists('output/Roland/policy_rank_score'):
+            os.makedirs('output/Roland/policy_rank_score')
+        res_dict = {}
+        res_dict['source'] = "First confirmed case\\policy strictness score and ranking.dta"
+        res_dict["name"] = "DataSet"
+        res_dict['data'] = {}
+        for key1, group1 in group.groupby('monthly_group'):
+            tmp_json = group[['iso', 'policy', 'score', 'rank']].to_dict(orient='records')
+            res_dict['data'][key1] = {}
+            res_dict['data'][key1]['label'] = key1
+            res_dict['data'][key1]['data'] = tmp_json
+        res_json = json.dumps(res_dict,indent=1)
+        f2 = open('output/Roland/policy_rank_score/' + key + '.json', 'w')
+        f2.write(res_json)
+        f2.close()
 
 
 def re_construct_top_policy():
@@ -166,5 +187,6 @@ if __name__ == '__main__':
     population = population.rename(columns={'country': 'Country', 'iso': 'ISO'})
 
     re_construct_covid_forecast()
+    # re_construct_policy_rank_score()
     # re_construct_top_policy()
     # re_construct_top_indicator()
